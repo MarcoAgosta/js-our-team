928a99810d16152b2d5dac8417634b403065ac19
MarcoAgosta <agostamarco@libero.it>
2022-11-15 15:28:01 +0100

    Raggiunta milestone 1

--- JS/main.js
+++ JS/main.js
@@ -1,7 +1,38 @@
 const membriTeam = [
-    membroprimo = {
+    primo = {
         nome: "Wayne Barnett",
         ruolo: "Founder & CEO",
         foto: "wayne-barnett-founder-ceo.jpg"
-    }
+    },
+    secondo = {
+        nome: "Angela Caroll",
+        ruolo: "Chief Editor",
+        foto: "angela-caroll-chief-editor.jpg"
+    },
+    terzo = {
+        nome: "Walter Gordon",
+        ruolo: "Office Manager",
+        foto: "walter-gordon-office-manager.jpg"
+    },
+    quarto = {
+        nome: "Angela Lopez",
+        ruolo: "Social Media Manager",
+        foto: "angela-lopez-social-media-manager.jpg"
+    },
+    quinto = {
+        nome: "Scott Estrada",
+        ruolo: "Developer",
+        foto: "scott-estrada-developer.jpg"
+    },
+    sesto = {
+        nome: "Barbara Ramos",
+        ruolo: "Graphic Designer",
+        foto: "barbara-ramos-graphic-designer.jpg"
+    },
 ]
+
+for (let i = 0; i <= membriTeam.length-1; i++){
+    console.log(membriTeam[i].nome)
+    console.log(membriTeam[i].ruolo)
+    console.log(membriTeam[i].foto)
+}
