9fbeed40e97c57322336dc85c921d1e07c136a50
MarcoAgosta <agostamarco@libero.it>
2022-11-16 12:30:59 +0100

    Alcune piccole modifiche

--- JS/main.js
+++ JS/main.js
@@ -1,30 +1,30 @@
 const membriTeam = [
-    primo = {
+    {
         nome: "Wayne Barnett",
         ruolo: "Founder & CEO",
         foto: "wayne-barnett-founder-ceo.jpg"
     },
-    secondo = {
+    {
         nome: "Angela Caroll",
         ruolo: "Chief Editor",
         foto: "angela-caroll-chief-editor.jpg"
     },
-    terzo = {
+    {
         nome: "Walter Gordon",
         ruolo: "Office Manager",
         foto: "walter-gordon-office-manager.jpg"
     },
-    quarto = {
+    {
         nome: "Angela Lopez",
         ruolo: "Social Media Manager",
         foto: "angela-lopez-social-media-manager.jpg"
     },
-    quinto = {
+    {
         nome: "Scott Estrada",
         ruolo: "Developer",
         foto: "scott-estrada-developer.jpg"
     },
-    sesto = {
+    {
         nome: "Barbara Ramos",
         ruolo: "Graphic Designer",
         foto: "barbara-ramos-graphic-designer.jpg"
@@ -37,5 +37,5 @@ for (let i = 0; i <= membriTeam.length-1; i++){
     console.log(membriTeam[i].nome)
     console.log(membriTeam[i].ruolo)
     console.log(membriTeam[i].foto)
-    lista.innerHTML += `<li>Il primo impiegato si chiama ${membriTeam[i].nome}, il suo ruolo è ${membriTeam[i].ruolo} e il file della sua foto profilo è ${membriTeam[i].foto}.</li>`
+    lista.innerHTML += `<li class="list-group-item">Il primo impiegato si chiama ${membriTeam[i].nome}, il suo ruolo è ${membriTeam[i].ruolo} e il file della sua foto profilo è ${membriTeam[i].foto}.</li>`
 }
